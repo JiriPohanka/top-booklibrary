1b9d152d01ee7f1957f3d634e1ead81fcaa27a74
Jiri Pohanka <jiri.pohanka@gmail.com>
2021-08-01 02:06:53 +0200

    Refactor code for fewer DOM manimapulations and less spaghetti

--- app.js
+++ app.js
@@ -1,63 +1,45 @@
 const newBookBtn = document.querySelector('.create-book-btn')
 const bookGrid = document.querySelector('.book-grid')
-const formSection = document.querySelector('.form-section')
-const bookLibrary = [{title: 'Martian', author: 'Ray Bradbury', pageCount: 349, status: 'no'},{title: 'Martian', author: 'Ray Bradbury', pageCount: 349, status: 'no'}]
+const formPopup = document.querySelector('.form-popup')
+const newBookForm = document.querySelector('#new-book-form')
+const bookDetailPopup = document.querySelector('.book-detail-popup')
+const bookLibrary = [
+    { title: 'Martian', author: 'Ray Bradbury', pageCount: 1, status: 'no' },
+    { title: 'Lord of The Rings', author: 'J.R.R. Tolkien', pageCount: 2, status: 'no' },
+    { title: 'Mistr a Markétka', author: 'N. Bulkagov', pageCount: 3, status: 'no' },
+    { title: 'Idiot', author: 'I. Dostoyevsky', pageCount: 4, status: 'no' },
+    { title: 'Anna Karenina', author: 'N. Tolstoy', pageCount: 5, status: 'no' },
+    { title: 'Last Meheecan', author: 'Butters', pageCount: 6, status: 'no' }
+]
 
-const createNewBook = () => {
-    const formWrapper = document.createElement('div');
-    formSection.appendChild(formWrapper);
-    formWrapper.innerHTML = 
-    `<form id="new-book-form" class="new-book-form">
-        <label name="title">Title</label>
-        <input name="title" id="title" placeholder="Lord of the Rings" type="text">
-        <label name="author">Author</label>
-        <input name="author" id="author" placeholder="J.R.R. Tolkien" type="text">
-        <label name="page-count">Page count</label>
-        <input name="page-count" id="page-count" placeholder="475" type="number">
-        <label name="status">Finished reading?</label>
-        <input name="status" id="status" placeholder="no" type="text">
-        <input type="submit" id="submit-book-btn" value="Add new book">
-    </form>`
+// when + button gets clicked //
+newBookBtn.addEventListener('click', createNewBook);
 
-    const newBookForm = document.querySelector('#new-book-form');
-
-
-    newBookForm.addEventListener('submit', (e) => {
-        e.preventDefault();
-        const title = e.target.elements.title.value;
-        const author = e.target.elements.author.value;
-        const pageCount = e.target.elements['page-count'].value;
-        const status = e.target.elements.status.value;
-        console.log(e, title, author, pageCount, status);
-
-        const book = new Book(title, author, pageCount, status)
-        bookLibrary.push(book)
-        book.logCreation()
-        console.log(bookLibrary);
-        cleanBookGrid();
-        populateBookGrid(bookLibrary);
-    })
+// after clicking the button to create a new book //
+function createNewBook () {
+    generateFormPopup();
+    newBookBtn.disabled = true;
+    newBookForm.addEventListener('submit', submitForm);
 }
 
-const cleanBookGrid = () => {
-    document.querySelector('.book-grid').innerHTML = "";
+const submitForm = (e) => {
+    newBookForm.removeEventListener('submit', submitForm);
+    e.preventDefault();
+    let title = e.target.elements.title.value;
+    let author = e.target.elements.author.value;
+    let pageCount = e.target.elements['page-count'].value;
+    let status = e.target.elements.status.value;
+
+    bookLibrary.push(new Book(title, author, pageCount, status));
+
+    populateBookGrid();
+    newBookForm.reset();
+    formPopup.classList.toggle('show');
+    newBookBtn.disabled = false;
+    newBookBtn.addEventListener('click', createNewBook);
 }
 
-const populateBookGrid = (arr) => {
-    for (let book of bookLibrary) {
-        function createBookSheet(book) {
-            const bookSheetWrap = document.createElement('div');
-            document.querySelector('.book-grid').appendChild(bookSheetWrap);
-            bookSheetWrap.innerHTML = `<p>${book.title}</p>
-            <p>${book.author}</p>
-            <p>${book.pageCount} pages</p>
-            <p>have read?: ${book.status}</p>`
-        }
-        createBookSheet(book);
-    }
-}
-
-function Book (title, author, pageCount, status) {
+function Book(title, author, pageCount, status) {
     this.title = title
     this.author = author
     this.pageCount = pageCount
@@ -66,9 +48,87 @@ function Book (title, author, pageCount, status) {
 
 Book.prototype.logCreation = () => console.log('new book has been added');
 
+const generateFormPopup = () => {
+    formPopup.classList.toggle('show');
+}
 
-newBookBtn.addEventListener('click', (e) => createNewBook())
+const generateBookDetailPopup = () => {
+    bookDetailPopup.classList.toggle('show');
+}
 
+const attachEventListeners = () => {
+    const detailButtons = document.querySelectorAll('.detail-book-btn');
+    const deleteButtons = document.querySelectorAll('.delete-book-btn');
 
-// a plus button onclick pops up a form for book insertion
-// form contains title, author, number of pages, reading status
+    for (let detailButton of detailButtons) {
+        detailButton.addEventListener('click', (e) => {
+            const currentBookObj = bookLibrary[e.target.dataset.bookId];
+            const index = e.target.dataset.bookId;
+            showBookDetail(index, currentBookObj);
+        })
+    }
+
+    for (let deleteButton of deleteButtons) {
+        deleteButton.addEventListener('click', (e) => {
+            bookLibrary.splice(e.target.dataset.bookId, 1);
+        })
+        deleteButton.addEventListener('click', () => populateBookGrid());
+    }
+}
+
+const showBookDetail = (i, obj) => {
+    newBookBtn.disabled = false
+    generateBookDetailPopup();
+    bookDetail = document.querySelector('.book-detail');
+    bookDetail.innerHTML =
+        `
+    <p>${obj.title}</p>
+    <p>${obj.author}</p>
+    <p>${obj.pageCount} pages</p>
+    <p>have read?: ${obj.status}</p>
+    <button data-book-id="${i}" class="edit-book-btn">edit</button>
+    <button data-book-id="${i}" class="btn delete-book-btn">delete</button>
+    `
+    const editButton = document.querySelector('div.book-detail-popup button.edit-book-btn');
+    const deleteButton = document.querySelector('div.book-detail-popup button.delete-book-btn');
+
+    deleteButton.addEventListener('click', () => bookLibrary.splice(i, 1));
+    deleteButton.addEventListener('click', () => bookDetailPopup.classList.toggle('show'));
+    deleteButton.addEventListener('click', () => populateBookGrid());
+}
+
+const cleanBookGrid = () => {
+    document.querySelector('.book-grid').innerHTML = "";
+}
+
+const populateBookGrid = () => {
+    cleanBookGrid();
+    for (let [i, book] of bookLibrary.entries()) {
+        createBookSheet(i, book);
+    }
+    attachEventListeners();
+}
+
+function createBookSheet(i, obj) {
+    const bookSheetWrap = document.createElement('div');
+    bookSheetWrap.setAttribute("data-book-id", i)
+    document.querySelector('.book-grid').appendChild(bookSheetWrap);
+    bookSheetWrap.innerHTML =
+        `
+        <p>${obj.title}</p>
+        <p>${obj.author}</p>
+        <p>${obj.pageCount} pages</p>
+        <p>have read?: ${obj.status}</p>
+        <button data-book-id="${i}" class="detail-book-btn">detail</button>
+        <button data-book-id="${i}" class="btn delete-book-btn">delete</button>
+        `
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    populateBookGrid();
+    newBookBtn.addEventListener('click', populateBookGrid);
+    formPopup.querySelector('.blocker').addEventListener('click', () => formPopup.classList.toggle('show'));
+    formPopup.querySelector('.blocker').addEventListener('click', () => newBookBtn.disabled = false);
+    bookDetailPopup.querySelector('.blocker').addEventListener('click', () => bookDetailPopup.classList.toggle('show'));
+    bookDetailPopup.querySelector('.blocker').addEventListener('click', () => newBookBtn.disabled = false);
+});
